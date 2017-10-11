55b639e7f37453c0cbb404f278ccf1531cf1d2b9
Transfernet <30530720+Transfernet@users.noreply.github.com>
2017-10-11 14:07:08 -0400

    Removed time dependence

--- Lottery.sol
+++ Lottery.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.17;
 
 contract Owned {
     address public owner;
@@ -12,104 +12,78 @@ contract Owned {
         _;
     }
     
-    function transferOwnership(address newOwner) public onlyOwner {
+    function transferOwnership(address newOwner) public {
         owner = newOwner;
     }
 }
 
 contract Lottery is Owned {
     
-    struct Ticket {
-        address owner;
-        bool isBought;
-        uint timeOfPurchase;
-    }
-    Ticket[] tickets;  //Might need to somehow initialize this array to a size
+    address[] public tickets;  //Might need to somehow initialize this array to a size
     //Used mostly for reset of the lottery so we don't have to search the
     //full array to reset the isBought bools
-    uint[] boughtTickets;
-    uint numBought;
+    uint public numBought;
     
-    uint public startTime;    //Maybe use lower uints (this is 256)
-    uint public minLotteryLength;  // = 2 weeks TODO
     uint public etherPool;    //In wei
     uint public minJackpot;   //In wei
     uint public ticketPrice;  //In wei
     uint public numTickets;
     
     uint etherFundingPool;
-    bool timeLimitReached;
     bool jackPotReached;
     
     event LotteryEnded(bool winnerExists, address winner, uint prize);
     event NewLotteryBegun(uint prize);
     event MinJackpotReached();
     
-    function Lottery(uint _lotteryLength, uint _minJackpot, 
-                        uint _ticketPrice, uint _numTickets) public {
+    function Lottery(uint _minJackpot, uint _ticketPrice, uint _numTickets) public {
         numBought = 0;
         
-        startTime = now;
-        minLotteryLength = _lotteryLength;
         etherPool = 0;
         minJackpot = _minJackpot;
         ticketPrice = _ticketPrice;
         numTickets = _numTickets;
         
-        timeLimitReached = false;
         jackPotReached = false;
     }
     
-    function buyTickets(uint[] _tickets) public payable {  
+    function buyTickets() public payable {  
         //Make sure they are paying exact change for a number of tickets and 
         //that the lottery is currently being held
-        require(now <= startTime + minLotteryLength);
-        require(msg.value >= _tickets.length * ticketPrice);
+        require(msg.value >= ticketPrice);
         
-        uint change = msg.value - _tickets.length * ticketPrice;
+        //uint change = msg.value % ticketPrice;
+        uint ticketsBought = msg.value / ticketPrice;
+        //if ((ticketsBought + numBought) > numTickets) {
+        //    change += (ticketsBought + numBought - numTickets) * ticketPrice;
+        //}
         
-        for (uint i=0; i<_tickets.length; i++) {
-            require(_tickets[i] < numTickets);
-            if (!tickets[_tickets[i]].isBought) {
-                tickets[_tickets[i]].owner = msg.sender;
-                tickets[_tickets[i]].isBought = true;
-                tickets[_tickets[i]].timeOfPurchase = now;
-                boughtTickets[numBought] = _tickets[i];
-                numBought++;
-            } else {
-                //Tell the purchaser somehow TODO
-                //Send them back the money for that ticket
-                msg.sender.transfer(change);
+        for (uint i=0; i<ticketsBought; i++) {
+            tickets[numBought] = msg.sender;
+            numBought++;
+            if (numBought >= numTickets) {
+                break;
             }
         }
         
+        //if (change > 0) {
+        //    msg.sender.transfer(change);
+        //}
+        
         //Reflect the purchases in the pool
-        etherPool += msg.value * 9 / 10;
-        etherFundingPool += msg.value / 10;
-        if (change != 0) {
-            msg.sender.transfer(change);
-        }
+        etherPool += ((msg.value * 9) / 10);
+        etherFundingPool += (msg.value / 10);
         
         //Check to trigger event for reaching minimum pool
         if(!jackPotReached && (etherPool > minJackpot)) {
             jackPotReached = true;
             MinJackpotReached();   //Call event
-            if (timeLimitReached) {
-                endLottery();
-            }
         }
     }
     
     function endLottery() public onlyOwner {
-        //Ensure it's time to end
-        require(now >= startTime + minLotteryLength);  
-        //TODO These timeLimitReached and minJackpotReached bools might need work
         
-        //Check that the pool reached the minimum for payout
-        if (etherPool < minJackpot) {
-            timeLimitReached = true;
-            return;
-        }
+        require(jackPotReached);
         
         /////////////////////////////////////////////
         //Take a current or specific block hash and//
@@ -120,11 +94,9 @@ contract Lottery is Owned {
         
         address winner = 0; //TODO do we need both winner and isWinner?
         bool isWinner = false;
-        for (uint i=0; i<numBought; i++) {
-            if (boughtTickets[i] == winningTicket) {
-                winner = tickets[winningTicket].owner;
-                isWinner = true;
-            }
+        if (winningTicket < numBought) {
+            winner = tickets[winningTicket];
+            isWinner = true;
         }
         
         LotteryEnded(isWinner, winner, etherPool);
@@ -138,28 +110,25 @@ contract Lottery is Owned {
         
     }
     
-    function restartLottery(uint _minLotteryLength, 
-                            uint _minJackpot, 
+    function restartLottery(uint _minJackpot, 
                             uint _ticketPrice, 
-                            uint _numTickets) public onlyOwner {  //Needs work TODO
+                            uint _numTickets) public onlyOwner {  
         //Reset all the values
-        startTime = now;
-        minLotteryLength = _minLotteryLength;
         minJackpot = _minJackpot;   
         ticketPrice = _ticketPrice;  
         numTickets = _numTickets;
     
-        timeLimitReached = false;
         jackPotReached = false;
     
         //Reset the tickets
-        for (uint i=0; i<numBought; i++) {
-            tickets[boughtTickets[i]].isBought = false;
-        }
         numBought = 0;
     
         //Alert watchers
         NewLotteryBegun(etherPool);
+    }
+    
+    function Die() public onlyOwner {
+        selfdestruct(owner);
     }
     
 }
